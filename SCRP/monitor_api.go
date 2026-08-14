768a995774e8151997176bf8ae4274ce3d8df3eb
visa <mygoonet@gmail.com>
2026-08-14 17:42:28 +0800

    22

--- SCRP/monitor_api.go
+++ SCRP/monitor_api.go
@@ -373,7 +373,7 @@ func MonitorAPI(browser *Browser, cfg Config, tel *TelegramClient, cmdCh <-chan 
 
 	startTransportationsCapture(ctx)
 
-	interval := 360 * time.Second
+	interval := 10 * time.Second
 	autoSign := true
 	giveUp := map[string]bool{}
 
